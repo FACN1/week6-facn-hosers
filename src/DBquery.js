407c6a51737ee1f6877b8dc1b1f0a676f17ea7fc
Lawson <lgbeaty@live.ca>
2017-03-31 10:54:48 +0300

    split DBquery.js so requests to the database are handled in seperate functions

--- src/DBquery.js
+++ src/DBquery.js
@@ -1,22 +1,21 @@
 const dbConnection = require('../database/db_connection.js');
 
-function getData(search, cb){
-  if (!search){
-    dbConnection.query('SELECT shop_name, shop_rating, cost, address, description, tags FROM shops;', (err, res) => {
-      if (err) cb(err);
-      cb(null, res.rows);
-    });
-  }
-  else {
-
-    dbConnection.query(`SELECT shop_name, shop_rating, cost, address, description,tags FROM shops WHERE tags LIKE '%${search}%';`,
-     (err, res) => {
-      if (err) cb(err);
-      cb(null, res.rows);
-    });
-  };
+function getAllData(cb){
+  dbConnection.query('SELECT shop_name, shop_rating, cost, address, description, tags FROM shops;', (err, res) => {
+    if (err) cb(err);
+    cb(null, res.rows);
+  });
 }
 
+function getSearchData(search, cb){
+  dbConnection.query(`SELECT shop_name, shop_rating, cost, address, description,tags FROM shops WHERE tags LIKE '%${search}%';`,
+   (err, res) => {
+    if (err) cb(err);
+    cb(null, res.rows);
+  });
+};
+
 module.exports = {
-  getData: getData
+  getAllData: getAllData,
+  getSearchData: getSearchData
 }
